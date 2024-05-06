17bd3e05ed7879d72fc6c2757327337f89a36198
unknown <mani.2280@gmail.com>
2024-05-06 20:06:53 +0530

    screen shot

--- Tester6/src/test/java/com/pack2/M1.java
+++ Tester6/src/test/java/com/pack2/M1.java
@@ -1,27 +1,27 @@
-package com.pack1a;
+package com.pack2;
 
 import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class M1 {
 	
-	public static void main(String[] args) throws InterruptedException {
+	@Test
+	public void test() throws InterruptedException
+	{
 		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+		driver.get("file:///D:/Development/repo/javaSeleniumChinchuVeena/BasicHtmlElement.html");
 		Thread.sleep(1000);
-		
-		Thread.sleep(1000);
-		driver.switchTo().alert().accept();
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//input[@value='testing']")).click();
+		driver.findElement(By.id("firstname")).sendKeys("admin");
 		Thread.sleep(1000);
 		driver.quit();
 	}
+	
 
 }
