de9c7ab4cbc65952309db907172a5b1a7c35bc8f
unknown <mani.2280@gmail.com>
2024-05-08 19:30:45 +0530

    proj 2 to 4

--- Tester6/src/test/java/com/pack3/Tester3a.java
+++ Tester6/src/test/java/com/pack3/Tester3a.java
@@ -1,5 +1,7 @@
 package com.pack3;
 
+import java.io.File;
+
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
@@ -9,7 +11,8 @@ public class Tester3a extends SuperTest{
 	public void test() throws InterruptedException
 	{
 		driver.get("file:///C:/Users/tester_android/Desktop/MorningBatch01/BasicHtmlElement.html");
-		driver.findElement(By.name("resume")).sendKeys("someFilePath");
+		driver.findElement(By.name("resume")).sendKeys(System.getProperty("user.dir") + File.separator + "Downloads"+
+				File.separator +"menu.pdf");
 		Thread.sleep(5000);
 	}
 
